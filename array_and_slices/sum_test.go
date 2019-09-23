ba6f2be25e31e246bad41ac3786ac0119aa9f73e
Manassarn Manoonchai <manassarn@gmail.com>
2019-09-23 22:24:29 +0700

    :sparkles: SumAllTails

--- array_and_slices/sum_test.go
+++ array_and_slices/sum_test.go
@@ -35,3 +35,12 @@ func TestSumAll(t *testing.T) {
 		t.Errorf("Expected %v but got %v", want, got)
 	}
 }
+
+func TestSumAllTails(t *testing.T) {
+	got := SumAllTails([]int{1, 2}, []int{0, 9})
+	want := []int{2, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Expected %v but got %v", want, got)
+	}
+}
