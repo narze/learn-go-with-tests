ca97a106612e649556d01e7c3693eb45140556b2
Manassarn Manoonchai <manassarn@gmail.com>
2019-10-09 16:37:12 +0900

    :recycle: [Pointers & Errors] Refactor error message with global var

--- pointers_and_errors/wallet_test.go
+++ pointers_and_errors/wallet_test.go
@@ -12,14 +12,14 @@ func TestWallet(t *testing.T) {
 		}
 	}
 
-	assertError := func(t *testing.T, got error, want string) {
+	assertError := func(t *testing.T, got error, want error) {
 		t.Helper()
 
 		if got == nil {
 			t.Fatal("didn't get an error but wanted one")
 		}
 
-		if got.Error() != want {
+		if got != want {
 			t.Errorf("got %q, want %q", got, want)
 		}
 	}
@@ -46,6 +46,6 @@ func TestWallet(t *testing.T) {
 		err := wallet.Withdraw(Bitcoin(100))
 
 		assertBalance(t, wallet, startingBalance)
-		assertError(t, err, "cannot withdraw, insufficient funds")
+		assertError(t, err, ErrInsufficientFunds)
 	})
 }
